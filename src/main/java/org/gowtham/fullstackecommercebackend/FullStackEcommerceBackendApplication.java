66b1610c188e49f736289e73658554ca10c3d211
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-07-31 10:19:26 +0530

    Initial commit

---
+++ src/main/java/org/gowtham/fullstackecommercebackend/FullStackEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package org.gowtham.fullstackecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackEcommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackEcommerceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gowtham/fullstackecommercebackend/FullStackEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gowtham.fullstackecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackEcommerceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
